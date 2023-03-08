aa77af6e6f8b3218f5aa69ef7d1796c98addb704
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-03-08 01:09:14 -0500

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+const names =["Charlie", "Samip", "Ali"];
+const eventName="birthday";
+
+
+function writeCards(names,eventName)
+{    const giftMessage=[];
+     let temp;
+     for(let i=0;i<names.length;i++)
+     {
+       temp= `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+       giftMessage.push(temp);
+
+     }
+    
+   return giftMessage;
+}
+
+
+function countDown()
+{
+    let number=10;
+    while(number>=0)
+    {
+        console.log(number);
+        number--;
+    }
+}
